4c50b2593cd2ee1e7d6113a79c79f759622a60af
Thels75 <davethels@hotmail.com>
2025-04-09 15:46:04 +0100

    changes 3

--- 03a_JavaScriptConditionals/starter/index.js
+++ 03a_JavaScriptConditionals/starter/index.js
@@ -1,2 +1,55 @@
+let age = 42
+if (age <= 17) { 
+    console.log("Underage"); 
+} 
+else { 
+    console.log("18 or over");
+ }
+ if (age <= 17) { 
+    console.log("Underage"); 
+} 
+else if 
+(age <=65) 
+{ 
+console.log("Insurable"); 
 
+} 
+else { console.log("out of range"); 
 
+}
+if (age <= 17) { 
+    console.log("Underage"); 
+} 
+else if 
+(age <= 50) 
+{ 
+console.log("Insurable"); 
+
+} 
+else { console.log("out of range"); 
+
+}
+if (age <= 17) { 
+    console.log("Underage"); 
+} 
+else if 
+(age <=10) 
+{ 
+console.log("Insurable"); 
+
+} 
+else { console.log("out of range"); 
+
+}
+if (age <= 17) { 
+    console.log("Underage"); 
+} 
+else if 
+(age <= 80) 
+{ 
+console.log("Insurable"); 
+
+} 
+else { console.log("out of range"); 
+
+}
